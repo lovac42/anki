3ca737614ca1f8a03acd5383d707ded1ef22d518
Arthur Milchior <Arthur@Milchior.fr>
2019-06-15 15:49:31 +0200

    commenting python code

--- anki/collection.py
+++ anki/collection.py
@@ -1095,6 +1095,7 @@ and type = {CARD_NEW}""", [intTime(), self.usn()])
         newSize = os.stat(self.path)[stat.ST_SIZE]
         txt = _("Database rebuilt and optimized.")
         ok = not problems
+        print("Adding in collection.py")
         problems.append(txt)
         # if any problems were found, force a full sync
         if not ok:
