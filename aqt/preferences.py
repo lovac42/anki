c5903cdd6ad3aa6fc8460ba3c4752b8fd2623aff
Arthur Milchior <Arthur@Milchior.fr>
2019-07-01 13:04:24 +0200

    Change note types without sync

--- aqt/preferences.py
+++ aqt/preferences.py
@@ -272,6 +272,7 @@ Not currently enabled; click the sync button in the main window to enable."""))
         storeValue[name] = value
 
     extraOptions = [
+        ("changeModelWithoutFullSync", False, True, False),
     ]
 
     def setupExtra(self):
