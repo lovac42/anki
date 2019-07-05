e2c76ab05c5d9791c19de5e92b5fed0ca826cfec
Arthur Milchior <Arthur@Milchior.fr>
2019-07-06 01:51:50 +0200

    Long term back-up

--- anki/incorporatedAddons.py
+++ anki/incorporatedAddons.py
@@ -14,6 +14,7 @@ class Addon:
 
 """ Set of characteristic of Add-ons incorporated here"""
 incorporatedAddonsSet = {
+    Addon("Long term backups", 529955533, 1562981271, "92fd0e93ee586912ae7736b03ab9a549db55a807", "https://github.com/Arthur-Milchior/anki-old-backup"),
 }
 
 incorporatedAddonsDict = {**{addon.name: addon for addon in incorporatedAddonsSet if addon.name},
