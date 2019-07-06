9387059db9e7a5a80a7bce54e9944b6a32572926
Arthur Milchior <Arthur@Milchior.fr>
2019-07-06 03:21:11 +0200

    Deal with incorporated addons

---
+++ anki/incorporatedAddons.py
@@ -0,0 +1,20 @@
+
+## Add-ons incorporated in this fork.
+
+class Addon:
+    def __init__(self, name = None, id = None, mod = None, gitHash = None, gitRepo = None):
+        self.name = name
+        self.id = id
+        self.mod = mod
+        self.gitHash = gitHash
+        self.gitRepo = gitRepo
+
+    def __hash__(self):
+        return self.id or hash(self.name)
+
+""" Set of characteristic of Add-ons incorporated here"""
+incorporatedAddonsSet = {
+}
+
+incorporatedAddonsDict = {**{addon.name: addon for addon in incorporatedAddonsSet if addon.name},
+                          **{addon.id: addon for addon in incorporatedAddonsSet if addon.id}}
