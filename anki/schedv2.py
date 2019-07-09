cebbd6a5d5c01fa126eda656ce180b1ce8a583ed
Arthur Milchior <Arthur@Milchior.fr>
2019-07-09 18:01:12 +0200

    Adding constants for leech actions

--- anki/schedv2.py
+++ anki/schedv2.py
@@ -1129,7 +1129,7 @@ where id = ?
             f.flush()
             # handle
             a = conf['leechAction']
-            if a == 0:
+            if a == LEECH_SUSPEND:
                 card.queue = QUEUE_SUSPENDED
             # notify UI
             runHook("leech", card)
