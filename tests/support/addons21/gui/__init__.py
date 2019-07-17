ca3cd1b9b5f9cd720c4b3e7d9893b021178ad524
Arthur Milchior <Arthur@Milchior.fr>
2019-07-17 10:53:43 +0200

    Some test to try main gui options

--- tests/support/addons21/gui/__init__.py
+++ tests/support/addons21/gui/__init__.py
@@ -4,19 +4,41 @@ from anki.hooks import addHook
 import time
 import sys
 
+def ensureStudy():
+    mw.onStudyKey()
+    mw.overview._linkHandler("study")
+
+def doAndUndoInStudy(method, txt = ""):
+    ensureStudy()
+    method()
+    mw.onUndo()
+    time.sleep(1)
+    # if txt:
+    #     print(f"{txt} done")
+
 def test():
-    t = Thread(target=myfunc)
-    t.start
+    doAndUndoInStudy(lambda: [mw.reviewer._linkHandler("ans"),  mw.reviewer._linkHandler("ease2")], "ease2")
+    doAndUndoInStudy(mw.reviewer.onBuryNote, "bury note")
+    doAndUndoInStudy(mw.reviewer.onBuryCard, "bury card")
+    doAndUndoInStudy(mw.reviewer.onSuspend, "suspend note")
+    doAndUndoInStudy(mw.reviewer.onSuspendCard, "suspend card")
+    doAndUndoInStudy(mw.reviewer.onDelete, "delete")
+    doAndUndoInStudy(lambda: mw.reviewer.setFlag(1), "flag1")
+
+    ensureStudy()
+    edit = mw.onEditCurrent()
+    # print("after edit")
+
     browser = mw.onBrowse()
     addCard = mw.onAddCard()
     stats = mw.onStats()
     prefs = mw.onPrefs()
-    time.sleep(2)
+    # can't test options, because it blocks the script
+    time.sleep(1)
     # debug= mw.onDebug()
     # debug.close()
     mw.close()
-    print("end")
-    time.sleep(2)
+    # print("end")
 
 addHook("profileLoaded", test)
 
