353453515b9160d017aead4eda640f7d1922b610
Arthur Milchior <Arthur@Milchior.fr>
2019-07-18 03:24:14 +0200

    Correct name in deckbrowser's node

--- aqt/deckbrowser.py
+++ aqt/deckbrowser.py
@@ -180,7 +180,7 @@ where id > ?""", (self.mw.col.sched.dayCutoff-86400)*1000)
         cnt --  the number of sibling, counting itself
         nameMap -- dictionnary, associating to a deck id its node
         """
-        name, did, due, lrn, new, children = node
+        name, did, rev, lrn, new, children = node
         deck = self.mw.col.decks.get(did)
         if did == 1 and cnt > 1 and not children:
             # if the default deck is empty, hide it
@@ -194,7 +194,7 @@ where id > ?""", (self.mw.col.sched.dayCutoff-86400)*1000)
         prefix = "-"
         if self.mw.col.decks.get(did)['collapsed']:
             prefix = "+"
-        due += lrn
+        due = rev + lrn
         def indent():
             return "&nbsp;"*6*depth
         if did == self.mw.col.conf['curDeck']:
