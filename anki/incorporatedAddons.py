01916b7cf17ec779a7d2eb0807292109be31265f
Arthur Milchior <Arthur@Milchior.fr>
2019-11-05 15:57:19 +0100

    Adding the add-on "Direct to review"

--- anki/incorporatedAddons.py
+++ anki/incorporatedAddons.py
@@ -14,6 +14,7 @@ class Addon:
 
 """ Set of characteristic of Add-ons incorporated here"""
 incorporatedAddonsSet = {
+    Addon("Directly review, without going through overview page", 1024346707, gitHash="0a0d0365deb022c54197f90c3f1100e9fc259ec0", gitRepo="https://github.com/Arthur-Milchior/anki-direct-to-review"),
 }
 
 incorporatedAddonsDict = {**{addon.name: addon for addon in incorporatedAddonsSet if addon.name},
