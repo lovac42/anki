92d109dcf3c1a39417b637bcd164ee54f6010b98
Arthur Milchior <Arthur@Milchior.fr>
2019-11-17 16:36:09 +0100

    Preference browserFromReviewer

--- aqt/preferences.py
+++ aqt/preferences.py
@@ -227,6 +227,7 @@ Not currently enabled; click the sync button in the main window to enable."""))
         {"name":"numBackups", "kind":"numeric", "sync":False},
         # advanced option
         {"name":"pastePNG", "sync":False},
+        {"name":"browserFromReviewer", "default":"default", "fromCol": (lambda x: {"cid":0,"nid":1,"did":2, "default":3}.get(x, 1)), "toCol": (lambda x: {0:"cid",1:"nid",2:"did", 3:"default"}[x]), "kind": "combo"},
     ]
 
     def setupOneSetting(self, name, default=False, kind="check", sync=True, fromCol=identity, toCol=identity):
