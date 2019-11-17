1033a8844ecea3a48cf274032c4901751c9f1afc
Arthur Milchior <Arthur@Milchior.fr>
2019-11-17 01:25:18 +0100

    Factorize fixers

--- anki/fixing.py
+++ anki/fixing.py
@@ -33,6 +33,29 @@ class FixingManager:
         "ensureSomeNoteType",
     ]
 
+
+    def _template(self, query, messageByError, singular, plural, deletion=None, params=(), reason=None):
+        """Execute query. If it returns some value, it means there was a
+        problem. Append it to self.problems. Apply deletion to the
+        list returned by the query to correct the error.
+        query -- a sql query returning tuples with problem
+        messageByError -- a message to add to the list of problems for each result of the query. It takes as many parameter as there are query columns
+        singular, plurar -- a global message to tell all problems which occured
+        deletion -- a method to apply to the tuples found
+        reason -- not used currently
+        """
+        problems = []
+        tups = self.db.all(query, params)
+        for tup in tups:
+            problems.append(messageByError.format(*tup))
+        if tups:
+            problems.append(ngettext(singular,
+                                          plural, len(tups))
+                                 % len(tups))
+            if deletion:
+                deletion(tups)
+        return problems
+
     def fixIntegrity(self):
         "Fix possible problems and rebuild caches."
         problems = []
@@ -59,16 +82,12 @@ class FixingManager:
 
     def noteWithMissingModel(self):
         # note types with a missing model
-        ids = self.db.list(
+        return self._template(
             """select id from notes where mid not in """ + ids2str(self.col.models.ids()),
-        )
-        if ids:
-            self.col.remNotes(ids)
-            return [
-                ngettext("Deleted %d note with missing note type.",
-                         "Deleted %d notes with missing note type.", len(ids))
-                % len(ids)
-            ]
+            "Note {} removed because it has no known model",
+            "Deleted %d note with missing note type.",
+            "Deleted %d notes with missing note type.",
+            self.col.remNotes(ids))
 
     def override(self):
         # for each model
@@ -94,77 +113,66 @@ class FixingManager:
         # cards with invalid ordinal
         problems = []
         for model in self.col.models.all(MODEL_STD):
-            ids = self.db.list(
+            problems += self._template(
                 """select id from cards where ord not in %s and nid in ( select id
                 from notes where mid = ?)""" %
                 ids2str([template['ord'] for template in model['tmpls']]),
-                model['id'])
-            if ids:
-                problems.append(
-                    ngettext("Deleted %d card with missing template.",
-                             "Deleted %d cards with missing template.",
-                             len(ids)) % len(ids))
-                self.col.remCards(ids)
+                "Delete note {} because its template is missing",
+                "Deleted %d card with missing template.",
+                "Deleted %d cards with missing template.",
+                self.col.remCards
+                params=(model['id'],)
+            )
         return problems
 
     def wrongNumberOfField(self):
         # notes with invalid field count
         problems = []
         for model in self.col.models.all():
-            # notes with invalid field count
-            ids = self.db.execute(
-                """select id from notes where mid = ? and 
-                (LEN(flds)-LEN(REPLACE(flds, '\x1f', ''))) <> ?""", model['id'])
-            if ids:
-                problems.append(
-                    ngettext("Deleted %d note with wrong field count.",
-                             "Deleted %d notes with wrong field count.",
-                             len(ids)) % len(ids))
-                self.col.remNotes(ids)
+            problems += self._template(
+                """select id from notes where mid = ? and (LEN(flds)-LEN(REPLACE(flds, '\x1f', ''))) <> ?""",
+                "Delete note {} because it has a wrong number of fields.",
+                "Deleted %d note with wrong field count.",
+                "Deleted %d notes with wrong field count.",
+                self.col.remNotes,
+                params = (model['id'], len(model['flds'])))
         return problems
+
 
     def noteWithoutCard(self):
         # delete any notes with missing cards
-        ids = self.db.list(
+        return self._template(
             """select id from notes where id not in
             (select distinct nid from cards)""",
-        )
-        if ids:
-            cnt = len(ids)
-            self.col._remNotes(ids)
-            return [
-                ngettext("Deleted %d note with no cards.",
-                         "Deleted %d notes with no cards.", cnt) % cnt
-            ]
+            "Delete note {} because it has no cards",
+            "Deleted %d note with no cards.",
+            "Deleted %d notes with no cards.",
+            self.col._remNotes)
 
     def odueNotInDyn(self):
         # cards with odue set when it shouldn't be
-        ids = self.db.list(
+        self._template(
             f"""select id from cards where odue > 0 and (type={CARD_LRN} or
             queue={CARD_DUE}) and not odid""",
+            "Unset odue of card {} because its not filtered",
+            "Fixed %d card with invalid properties.",
+            "Fixed %d cards with invalid properties.",
+            lambda ids: self.db.execute("update cards set odue=0 where id in "+
+                                        ids2str(ids))
         )
-        if ids:
-            cnt = len(ids)
-            self.db.execute("update cards set odue=0 where id in "+
-                ids2str(ids))
-            return [
-                ngettext("Fixed %d card with invalid properties.",
-                         "Fixed %d cards with invalid properties.", cnt) % cnt
-            ]
 
-    def odidNotInDyn(self):
+
+        def odidNotInDyn(self):
         # cards with odid set when not in a dyn deck
         dids = [id for id in self.col.decks.allIds() if not self.col.decks.isDyn(id)]
-        ids = self.db.list("""
-        select id from cards where odid > 0 and did in %s""" % ids2str(dids))
-        if ids:
-            cnt = len(ids)
-            self.db.execute("update cards set odid=0, odue=0 where id in "+
-                ids2str(ids))
-            return [
-                ngettext("Fixed %d card with invalid properties.",
-                         "Fixed %d cards with invalid properties.", cnt) % cnt
-            ]
+        return self._template("""
+        select id from cards where odid > 0 and did in %s""" % ids2str(dids),
+                      "unset odid, odue in card {} because not in dynamic deck",
+                      "Fixed %d card with invalid properties.",
+                      "Fixed %d cards with invalid properties.",
+                      lambda ids: self.db.execute("update cards set odid=0, odue=0 where id in "+
+                                                  ids2str(ids))
+                      )
 
     def tags(self):
         # tags
