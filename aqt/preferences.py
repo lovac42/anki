4c93e6ca5eb3d36e7ac171540a30da56da423569
Arthur Milchior <Arthur@Milchior.fr>
2019-11-21 04:22:58 +0100

    Preference: {window}MultipleTime

--- aqt/preferences.py
+++ aqt/preferences.py
@@ -227,6 +227,7 @@ Not currently enabled; click the sync button in the main window to enable."""))
         {"name":"numBackups", "kind":"numeric", "sync":False},
         # advanced option
         {"name":"pastePNG", "sync":False},
+        *[(f"{window}MultipleTime", True) for window in ["AddCards", "EditCurrent", "Browser", "OtherWindows"]],#those are the windows name, thus with caps
     ]
 
     def setupOneSetting(self, name, default=False, kind="check", sync=True, fromCol=identity, toCol=identity):
