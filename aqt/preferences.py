94b8c23d85f5adcf56f60c0543e8f29ae35d1fdc
Arthur Milchior <Arthur@Milchior.fr>
2019-11-21 04:21:04 +0100

    Preferece factorAddDay, factorRemoveDay

--- aqt/preferences.py
+++ aqt/preferences.py
@@ -227,6 +227,8 @@ Not currently enabled; click the sync button in the main window to enable."""))
         {"name":"numBackups", "kind":"numeric", "sync":False},
         # advanced option
         {"name":"pastePNG", "sync":False},
+        {"name": "factorAddDay", "kind":"numeric"},
+        {"name": "factorRemoveDay", "kind": "numeric"},
     ]
 
     def setupOneSetting(self, name, default=False, kind="check", sync=True, fromCol=identity, toCol=identity):
