0f484ccfd2a21c427d229f8721cf5e257d3bf8cb
Arthur Milchior <Arthur@Milchior.fr>
2019-11-25 11:20:20 +0100

    addTemplate in template

--- tests/test_find.py
+++ tests/test_find.py
@@ -44,7 +44,7 @@ def test_findCards():
     t = m.newTemplate("Reverse")
     t['qfmt'] = "{{Back}}"
     t['afmt'] = "{{Front}}"
-    mm.addTemplate(m, t)
+    t.add()
     m.save()
     f = deck.newNote()
     f['Front'] = 'test'
