b4a9ec1f68c4259ea44fb114ca3650fca28b6785
Arthur Milchior <Arthur@Milchior.fr>
2019-11-25 09:26:22 +0100

    availOrds in Model

--- anki/collection.py
+++ anki/collection.py
@@ -463,7 +463,7 @@ crt=?, mod=?, scm=?, dty=?, usn=?, ls=?, conf=?""",
     def findTemplates(self, note):
         "Return templates generating contents from this note."
         model = note.model()
-        avail = self.models.availOrds(model, joinFields(note.fields))
+        avail = model.availOrds(joinFields(note.fields))
         return self._tmplsFromOrds(model, avail)
 
     def _tmplsFromOrds(self, model, avail):
@@ -526,7 +526,7 @@ crt=?, mod=?, scm=?, dty=?, usn=?, ls=?, conf=?""",
             "select id, mid, flds from notes where id in "+snids):
             model = self.models.get(mid)
             assert(model)
-            avail = self.models.availOrds(model, flds)
+            avail = model.availOrds(flds)
             did = dids.get(nid) or model['did']
             due = dues.get(nid)
             # add any missing cards
