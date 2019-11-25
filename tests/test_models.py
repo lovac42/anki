25baf0a055a95b1c3f4f7dc875880d4c008e18e7
Arthur Milchior <Arthur@Milchior.fr>
2019-11-25 11:28:32 +0100

    moveTemplate in Template

--- tests/test_models.py
+++ tests/test_models.py
@@ -100,7 +100,7 @@ def test_templates():
     assert c.ord == 0
     assert c2.ord == 1
     # switch templates
-    d.models.moveTemplate(m, c.template(), 1)
+    c.template().move(1)
     reqSize(m)
     c.load(); c2.load()
     assert c.ord == 1
