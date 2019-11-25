6efeebc4f908578879fda6123f58553b1f824dec
Arthur Milchior <Arthur@Milchior.fr>
2019-11-25 09:13:22 +0100

    _reqForTemplate in Template

--- anki/models.py
+++ anki/models.py
@@ -622,48 +622,6 @@ select id from notes where mid = ?)""" % " ".join(map),
     # Required field/text cache
     ##########################################################################
 
-    def _reqForTemplate(self, model, flds, template):
-        """A rule which is supposed to determine whether a card should be
-        generated or not according to its fields.
-
-        See ../documentation/templates_generation_rules.md
-
-        """
-        ankiflagFlds = ["ankiflag"] * len(flds)
-        emptyFlds = [""] * len(flds)
-        data = [1, 1, model.getId(), 1, template['ord'], "", joinFields(ankiflagFlds), 0]
-        # The html of the card at position ord where each field's content is "ankiflag"
-        full = self.col._renderQA(data)['q']
-        data = [1, 1, model.getId(), 1, template['ord'], "", joinFields(emptyFlds), 0]
-        # The html of the card at position ord where each field's content is the empty string ""
-        empty = self.col._renderQA(data)['q']
-
-        # if full and empty are the same, the template is invalid and there is
-        # no way to satisfy it
-        if full == empty:
-            return "none", [], []
-        type = 'all'
-        req = []
-        for i in range(len(flds)):
-            tmp = ankiflagFlds[:]
-            tmp[i] = ""
-            data[6] = joinFields(tmp)
-            # if no field content appeared, field is required
-            if "ankiflag" not in self.col._renderQA(data)['q']:
-                req.append(i)
-        if req:
-            return type, req
-        # if there are no required fields, switch to any mode
-        type = 'any'
-        req = []
-        for i in range(len(flds)):
-            tmp = emptyFlds[:]
-            tmp[i] = "1"
-            data[6] = joinFields(tmp)
-            # if not the same as empty, this field can make the card non-blank
-            if self.col._renderQA(data)['q'] != empty:
-                req.append(i)
-        return type, req
 
     def availOrds(self, model, flds):
         """Given a joined field string, return ordinal of card type which
