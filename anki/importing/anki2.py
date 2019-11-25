3d8a017a5475865f320943992deccda0a6d8c3b7
Arthur Milchior <Arthur@Milchior.fr>
2019-11-26 00:47:02 +0100

    setId

--- anki/importing/anki2.py
+++ anki/importing/anki2.py
@@ -240,7 +240,7 @@ class Anki2Importer(Importer):
             if not self.dst.models.have(mid):
                 # copy it over
                 dstModel = Model(self.dst.models, srcModel.copy())
-                dstModel['id'] = mid
+                dstModel.setId(mid)
                 dstModel['usn'] = self.col.usn()
                 self.dst.models.update(dstModel)
                 break
@@ -251,7 +251,7 @@ class Anki2Importer(Importer):
                 # copy styling changes over if newer
                 if srcModel['mod'] > dstModel['mod']:
                     dstModel = Model(self.dst.models, srcModel.copy())
-                    dstModel['id'] = mid
+                    dstModel.setId(mid)
                     dstModel['usn'] = self.col.usn()
                     self.dst.models.update(dstModel)
                 break
