b97d51763288e0fe48b1c912bc7a3da7cd72b354
Arthur Milchior <Arthur@Milchior.fr>
2019-11-25 11:25:31 +0100

    _updateTemplOrds in Model

--- anki/storage.py
+++ anki/storage.py
@@ -212,7 +212,7 @@ def _upgradeClozeModel(col, model):
     for rem in rems:
         col.models.remTemplate(model, rem)
     del model['tmpls'][1:]
-    col.models._updateTemplOrds(model)
+    model.models._updateTemplOrds()
     model.save()
 
 # Creating a new collection
