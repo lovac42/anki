9cc6c588d3930cf72a0c53a8ef29e4c2cabad74f
Arthur Milchior <Arthur@Milchior.fr>
2019-11-25 11:20:20 +0100

    addTemplate in template

--- tests/test_schedv1.py
+++ tests/test_schedv1.py
@@ -57,7 +57,7 @@ def test_new():
     # t = m.newTemplate("Reverse")
     # t['qfmt'] = "{{Back}}"
     # t['afmt'] = "{{Front}}"
-    # mm.addTemplate(m, t)
+    # t.add()
     # m.save()
     # f = d.newNote()
     # f['Front'] = u"2"; f['Back'] = u"2"
@@ -803,11 +803,11 @@ def test_ordcycle():
     t = m.newTemplate("Reverse")
     t['qfmt'] = "{{Back}}"
     t['afmt'] = "{{Front}}"
-    mm.addTemplate(m, t)
+    t.add()
     t = m.newTemplate("f2")
     t['qfmt'] = "{{Front}}"
     t['afmt'] = "{{Back}}"
-    mm.addTemplate(m, t)
+    t.add()
     m.save()
     # create a new note; it should have 3 cards
     f = d.newNote()
