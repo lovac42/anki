7b6bb4ffdb6cb1bea4493570bb20a8e4e5562f6b
Arthur Milchior <Arthur@Milchior.fr>
2019-11-25 08:32:29 +0100

    seting perDay to 999999 in DConf

--- anki/dconf.py
+++ anki/dconf.py
@@ -5,5 +5,15 @@ class DConf(DictAugmentedIdUsn):
     """A configuration for decks
 
     """
+    def load(self, manager, dict):
+        super().load(manager, dict)
+        # set limits to within bounds
+        for type in ('rev', 'new'):
+            pd = 'perDay'
+            if self[type][pd] > 999999:
+                self[type][pd] = 999999
+                self.save()
+                self.manager.changed = True
+
     def isDefault(self):
         return str(self.getId()) == "1"
