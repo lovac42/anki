df19c0c707e9721587862ee7119b89b3a4e18627
Arthur Milchior <Arthur@Milchior.fr>
2019-11-26 08:59:09 +0100

    confId becomes newConf and returns a conf

--- tests/test_schedv2.py
+++ tests/test_schedv2.py
@@ -78,7 +78,7 @@ def test_newLimits():
             f.model()['did'] = g2.getId()
         d.addNote(f)
     # give the child deck a different configuration
-    c2 = d.decks.confId("new conf")
+    c2 = d.decks.newConf("new conf")
     g2.setConf(c2)
     d.reset()
     # both confs have defaulted to a limit of 20
@@ -375,8 +375,8 @@ def test_review_limits():
     parent = d.decks.byName("parent", create=True)
     child = d.decks.byName("parent::child", create=True)
 
-    pconf = d.decks.getConf(d.decks.confId("parentConf"))
-    cconf = d.decks.getConf(d.decks.confId("childConf"))
+    pconf = d.decks.newConf("parentConf")
+    cconf = d.decks.newConf("childConf")
 
     pconf['rev']['perDay'] = 5
     pconf.update()
