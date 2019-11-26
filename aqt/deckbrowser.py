b5d6ba6575dd1bd3ca9ccc3d55aa8aac5f1de16c
Arthur Milchior <Arthur@Milchior.fr>
2019-11-26 07:16:39 +0100

    Deck.rename

--- aqt/deckbrowser.py
+++ aqt/deckbrowser.py
@@ -283,7 +283,7 @@ where id > ?""", (self.mw.col.sched.dayCutoff-86400)*1000)
         if not newName or newName == oldName:
             return
         try:
-            self.mw.col.decks.rename(deck, newName)
+            deck.rename(newName)
         except DeckRenameError as e:
             return showWarning(e.description)
         self.show()
