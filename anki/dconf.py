3fff3294160ee283208eb1842fe6f6cffea55698
Arthur Milchior <Arthur@Milchior.fr>
2019-11-26 08:55:29 +0100

    DConf.copy

--- anki/dconf.py
+++ anki/dconf.py
@@ -1,4 +1,47 @@
-from anki.utils import DictAugmentedDyn
+import copy
+
+from anki.consts import *
+from anki.lang import _
+from anki.utils import DictAugmentedDyn, intTime
+
+defaultConf = {
+    'name': _("Default"),
+    'new': {
+        'delays': [1, 10],
+        'ints': [1, 4, 7], # 7 is not currently used
+        'initialFactor': STARTING_FACTOR,
+        'separate': True,
+        'order': NEW_CARDS_DUE,
+        'perDay': 20,
+        # may not be set on old decks
+        'bury': False,
+    },
+    'lapse': {
+        'delays': [10],
+        'mult': 0,
+        'minInt': 1,
+        'leechFails': 8,
+        # type 0=suspend, 1=tagonly
+        'leechAction': LEECH_SUSPEND,
+    },
+    'rev': {
+        'perDay': 200,
+        'ease4': 1.3,
+        'fuzz': 0.05,
+        'minSpace': 1, # not currently used
+        'ivlFct': 1,
+        'maxIvl': 36500,
+        # may not be set on old decks
+        'bury': False,
+        'hardFactor': 1.2,
+    },
+    'maxTaken': 60,
+    'timer': 0,
+    'autoplay': True,
+    'replayq': True,
+    'mod': 0,
+    'usn': 0,
+}
 
 
 class DConf(DictAugmentedDyn):
@@ -25,3 +68,19 @@ class DConf(DictAugmentedDyn):
 same id."""
         self.manager.dconf[str(self.getId())] = self
         self.manager.save()
+
+    def copy_(self, name):
+        """Create a new configuration and return its id.
+
+        Keyword arguments
+        cloneFrom -- The configuration copied by the new one."""
+        conf = self.deepcopy()
+        while 1:
+            id = intTime(1000)
+            if str(id) not in self.manager.dconf:
+                break
+        conf['id'] = id
+        conf.setName(name)
+        self.manager.dconf[str(id)] = conf
+        conf.save()
+        return conf
