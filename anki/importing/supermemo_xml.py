614f9fab913a79ece6791b93d2ae33f10d3c847f
Arthur Milchior <Arthur@Milchior.fr>
2019-11-26 01:10:56 +0100

    setName

--- anki/importing/supermemo_xml.py
+++ anki/importing/supermemo_xml.py
@@ -84,7 +84,7 @@ class SupermemoXmlImporter(NoteImporter):
         Pameters to be exposed to GUI are stored in self.META"""
         NoteImporter.__init__(self, *args)
         model = addBasicModel(self.col)
-        model['name'] = "Supermemo"
+        model.setName("Supermemo")
         self.col.models.save(model)
         self.initMapping()
 
