97909bc5613726f9b19018e22a27e1ea6801232a
Arthur Milchior <Arthur@Milchior.fr>
2019-11-27 09:21:25 +0100

    _updateRequired in model

--- anki/model.py
+++ anki/model.py
@@ -1,5 +1,6 @@
 import copy
 
+from anki.consts import *
 from anki.fields import Field
 from anki.templates import Template
 from anki.utils import DictAugmentedIdUsn
@@ -37,3 +38,18 @@ class Model(DictAugmentedIdUsn):
         """Generate all cards not yet generated, whose note's model is model.
         It's called only when model is saved, a new model is given and template is asked to be computed"""
         self.manager.col.genCards(self.nids())
+
+    # Required field/text cache
+    ##########################################################################
+
+    def _updateRequired(self):
+        """Entirely recompute the model's req value"""
+        if self['type'] == MODEL_CLOZE:
+            # nothing to do
+            return
+        req = []
+        flds = [fieldType['name'] for fieldType in self['flds']]
+        for template in self['tmpls']:
+            ret = template._req(flds)
+            req.append((template['ord'], ret[0], ret[1]))
+        self['req'] = req
