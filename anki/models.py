9840ffef1b2c0f4f104f1775f1ea63ff8e1973c6
Arthur Milchior <Arthur@Milchior.fr>
2019-11-27 09:21:25 +0100

    _updateRequired in model

--- anki/models.py
+++ anki/models.py
@@ -172,7 +172,7 @@ class ModelManager:
             model['mod'] = intTime()
             model['usn'] = self.col.usn()
             if updateReqs:
-                self._updateRequired(model)
+                model._updateRequired()
             if templates:
                 model._syncTemplates()
         self.changed = True
@@ -720,18 +720,6 @@ select id from notes where mid = ?)""" % " ".join(map),
 
     # Required field/text cache
     ##########################################################################
-
-    def _updateRequired(self, model):
-        """Entirely recompute the model's req value"""
-        if model['type'] == MODEL_CLOZE:
-            # nothing to do
-            return
-        req = []
-        flds = [fieldType.getName() for fieldType in model['flds']]
-        for template in model['tmpls']:
-            ret = template._req(flds)
-            req.append([template['ord'], ret[0], ret[1]])
-        model['req'] = req
 
     def _reqForTemplate(self, model, flds, template):
         """A rule which is supposed to determine whether a card should be
