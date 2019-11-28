c1191a5d84a09f3e148fcefd93a62420921fb11f
Arthur Milchior <Arthur@Milchior.fr>
2019-11-28 11:07:15 +0100

    Deck.addInManager

--- anki/decks.py
+++ anki/decks.py
@@ -208,7 +208,7 @@ class DeckManager:
         self.decks = {}
         for deck in json.loads(decks).values():
             deck = Deck(self, deck)
-            self.decks[str(deck['id'])] = deck
+            deck.addInManager()
         self.dconf = {}
         for dconf in json.loads(dconf).values():
             dconf = DConf(self, dconf)
@@ -284,7 +284,7 @@ class DeckManager:
             deck = deckToCopy.deepcopy()
         deck['name'] = name
         deck['id'] = id
-        self.decks[str(id)] = deck
+        deck.addInManager()
         self.save(deck)
         self.maybeAddToActive()
         runHook("newDeck")
@@ -420,7 +420,7 @@ class DeckManager:
 
     def update(self, deck):
         "Add or update an existing deck. Used for syncing and merging."
-        self.decks[str(deck['id'])] = deck
+        deck.addInManager()
         self.maybeAddToActive()
         # mark registry changed, but don't bump mod time
         self.save()
