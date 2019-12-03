de394aaa688df821ae570bbdc0ce395000d1a26e
Arthur Milchior <Arthur@Milchior.fr>
2019-12-03 14:46:05 +0100

    Deck._option

--- aqt/deck.py
+++ aqt/deck.py
@@ -2,4 +2,13 @@ import anki.deck
 
 
 class Deck(anki.deck.Deck):
-    pass
+    ## Deck Browser
+    @staticmethod
+    def _option():
+        """html cell, to show gear opening options"""
+        return f"""
+      <td align=center class=opts>
+        <a onclick='return pycmd("opts:{self.getId()}");'>
+          <img src='/_anki/imgs/gears.svg' class=gears>
+        </a>
+      </td>"""
