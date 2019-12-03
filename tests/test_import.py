e71e399fc27fec0a037a35bb6c988abc9cfe5dd9
Arthur Milchior <Arthur@Milchior.fr>
2019-12-03 14:56:31 +0100

    Deck.isLeaf

--- tests/test_import.py
+++ tests/test_import.py
@@ -1,115 +1,28 @@
 import aqt
 from anki import (__init__, cards, collection, consts, db, deck, decks, errors,
-                  exporting, find, hooks, lang, latex, media, model, models,
-                  mpv, notes, sched, schedv2, sound, stats, statsbg, stdmodels,
-                  storage, sync, tags, template, templates, utils)
+                  exporting, fields, find, hooks, lang, latex, media, model,
+                  models, mpv, notes, sched, schedv2, sound, stats, statsbg,
+                  stdmodels, storage, sync, tags, template, templates, utils)
 from anki.importing import (__init__, anki2, apkg, base, csvfile, mnemo,
                             noteimp, pauker, supermemo_xml)
 from anki.template import __init__, furigana, hint, template, view
-
-from aqt import about
-from aqt import addcards
-from aqt import addons
-from aqt import browser
-from aqt import clayout
-from aqt import customstudy
-from aqt import deck
-from aqt import deckbrowser
-from aqt import deckchooser
-from aqt import deckconf
-from aqt import decks
-from aqt import downloader
-from aqt import dyndeckconf
-from aqt import editcurrent
-from aqt import editor
-from aqt import errors
-from aqt import exporting
-from aqt import importing
-from aqt import __init__
-from aqt import main
-from aqt import mediasrv
-from aqt import modelchooser
-from aqt import models
-from aqt import overview
-from aqt import pinnedmodules
-from aqt import preferences
-from aqt import profiles
-from aqt import progress
-from aqt import qt
-from aqt import reviewer
-from aqt import sound
-from aqt import stats
-from aqt import studydeck
-from aqt import sync
-from aqt import tagedit
-from aqt import taglimit
-from aqt import toolbar
-from aqt import update
-from aqt import utils
-from aqt import webview
-
 from anki.utils import intTime, isMac, isWin
 from aqt import (__init__, about, addcards, addons, browser, clayout,
-                 customstudy, deckbrowser, deckchooser, deckconf, downloader,
-                 dyndeckconf, editcurrent, editor, errors, exporting, fields,
-                 importing, main, mediasrv, modelchooser, models, overview,
-                 pinnedmodules, preferences, profiles, progress, qt, reviewer,
-                 sound, stats, studydeck, sync, tagedit, taglimit, toolbar,
-                 update, utils, webview)
+                 customstudy, deck, deckbrowser, deckchooser, deckconf, decks,
+                 downloader, dyndeckconf, editcurrent, editor, errors,
+                 exporting, fields, importing, main, mediasrv, modelchooser,
+                 models, overview, pinnedmodules, preferences, profiles,
+                 progress, qt, reviewer, sound, stats, studydeck, sync,
+                 tagedit, taglimit, toolbar, update, utils, webview)
 
 if isWin:
     from aqt import winpaths
 
 
-from anki import cards
-from anki import collection
-from anki import consts
-from anki import db
-from anki import deck
-from anki import decks
-from anki import errors
-from anki import exporting
-from anki import fields
-from anki import find
-from anki import hooks
-from anki import __init__
-from anki import lang
-from anki import latex
-from anki import media
-from anki import model
-from anki import models
-from anki import mpv
-from anki import notes
-from anki import sched
-from anki import schedv2
-from anki import sound
-from anki import statsbg
-from anki import stats
-from anki import stdmodels
-from anki import storage
-from anki import sync
-from anki import tags
-from anki import template
-from anki import templates
-from anki import utils
 
 
 
-from anki.importing import anki2
-from anki.importing import apkg
-from anki.importing import base
-from anki.importing import csvfile
-from anki.importing import __init__
-from anki.importing import mnemo
-from anki.importing import noteimp
-from anki.importing import pauker
-from anki.importing import supermemo_xml
 
-from anki.template import furigana
-from anki.template import hint
-from anki.template import __init__
-from anki.template import template
-from anki.template import view
 
 def test_import():
     pass
