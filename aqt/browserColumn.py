b7b7d7c485ed8dfed7f1ec7756b589b6b7da33ce
Arthur Milchior <Arthur@Milchior.fr>
2019-12-07 20:06:01 +0100

    sorting in BrowserColumn

--- aqt/browserColumn.py
+++ aqt/browserColumn.py
@@ -3,8 +3,10 @@ class BrowserColumn:
     type -- the internal name of the column in the list of columns
     name -- the (translated) name of the column
     note -- whether this show informations relative to note. Otherwise it's to the card.
+    sort -- the text to use in finder to sort by this column. None means this columns can't be sorted.
     """
-    def __init__(self, type, name, note=None):
+    defaultSort = "note.id, card.ord"
+    def __init__(self, type, name, sort=None, note=None):
         """All methods names ends with BrowserColumn. Only the part before has to be given.
         If methodName is not indicated, then it's assumed to be the same as
         type. Except if it starts with card or note, in which case
@@ -18,6 +20,9 @@ class BrowserColumn:
         self.type = type
         self.name = name
         self.note = note
+        if sort is None:
+            sort = self.defaultSort
+        self.sort = sort
 
     def getBase(self, card):
         if self.note:
@@ -25,12 +30,16 @@ class BrowserColumn:
         else:
             return card
 
+    defaultSort = "note.id, card.ord"
+    def getSort(self):
+        return self.sort
+
 
 class ColumnByMethod(BrowserColumn):
     """
     methodName -- a method in the class card/note according to self.note.
     """
-    def __init__(self, type, name, methodName=None, note=None):
+    def __init__(self, type, name, sort=None, methodName=None, note=None):
         """All methods names ends with BrowserColumn. Only the part before has to be given.
         If methodName is not indicated, then it's assumed to be the same as
         type. Except if it starts with card or note, in which case
@@ -38,7 +47,7 @@ class ColumnByMethod(BrowserColumn):
         lower case.
 
         """
-        super().__init__(type, name, note)
+        super().__init__(type, name, sort, note)
         if methodName is not None:
             self.methodName = methodName
         else:
