b7b878fda68fcc08242524faacd463d6ebb1350c
Arthur Milchior <Arthur@Milchior.fr>
2019-12-09 07:16:06 -0800

    Browser.sortBackwards
    It becomes a parameter, so that it can be changed in a browser and
    stay the same in another one

--- aqt/browser.py
+++ aqt/browser.py
@@ -231,6 +231,7 @@ class DataModel(QAbstractTableModel):
         """Reverse the list of cards and update the display"""
         self.beginReset()
         self.cards.reverse()
+        self.browser.sortBackwards = not self.browser.sortBackwards        
         self.endReset()
 
     def saveSelection(self):
@@ -403,6 +404,7 @@ class Browser(QMainWindow):
         QMainWindow.__init__(self, None, Qt.Window)
         self.mw = mw
         self.col = self.mw.col
+        self.sortBackwards = self.col.conf['sortBackwards']
         self.lastFilter = ""
         self.focusTo = None
         self._previewWindow = None
@@ -541,6 +543,7 @@ class Browser(QMainWindow):
         self.mw.maybeReset()
         aqt.dialogs.markClosed("Browser")
         self._closeEventHasCleanedUp = True
+        self.col.conf['sortBackwards'] = self.sortBackwards
         self.mw.gcWindow(self)
         self.close()
 
@@ -771,11 +774,11 @@ by clicking on one on the left."""))
             # default to descending for non-text fields
             if type == "noteFld":
                 ord = not ord
-            self.col.conf['sortBackwards'] = ord
+            self.sortBackwards = ord
             self.search()
         else:
-            if self.col.conf['sortBackwards'] != ord:
-                self.col.conf['sortBackwards'] = ord
+            if self.sortBackwards != ord:
+                self.sortBackwards = ord
                 self.model.reverse()
         self.setSortIndicator()
 
@@ -788,7 +791,7 @@ by clicking on one on the left."""))
             hh.setSortIndicatorShown(False)
             return
         idx = self.model.activeCols.index(type)
-        if self.col.conf['sortBackwards']:
+        if self.sortBackwards:
             ord = Qt.DescendingOrder
         else:
             ord = Qt.AscendingOrder
