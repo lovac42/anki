c071e3b7318d910234e1650a09882e1b2c95031e
Arthur Milchior <Arthur@Milchior.fr>
2019-12-19 13:28:47 -0800

    bothSched._deckNewLimit

--- anki/bothSched.py
+++ anki/bothSched.py
@@ -196,3 +196,17 @@ did = ? and queue = {QUEUE_NEW} limit ?)""", did, lim)
             return True
         elif self.newCardModulus:
             return self.reps and self.reps % self.newCardModulus == 0
+
+    def _deckNewLimit(self, did, fn=None):
+        if not fn:
+            fn = self._deckNewLimitSingle
+        sel = self.col.decks.get(did)
+        lim = -1
+        # for the deck and each of its parents
+        for ancestor in [sel] + self.col.decks.parents(did):
+            rem = fn(ancestor)
+            if lim == -1:
+                lim = rem
+            else:
+                lim = min(rem, lim)
+        return lim
