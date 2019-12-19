83325831ee5ddda584b14dcdba744ecfca5622d4
Arthur Milchior <Arthur@Milchior.fr>
2019-12-19 01:41:33 -0800

    Remove flds from data

--- anki/cards.py
+++ anki/cards.py
@@ -224,12 +224,11 @@ lapses=?, left=?, odue=?, odid=?, did=? where id = ?""",
             note = self.note(reload)
             model = self.model()
             template = self.template()
-            data = [note.joinedFields()]
             if browser:
                 args = (template.get('bqfmt'), template.get('bafmt'))
             else:
                 args = tuple()
-            self._qa = self.col._renderQA(model.getId(), self.ord, data, self.id, note.id, self.originalDid(), note.stringTags(), self.flags, *args)
+            self._qa = self.col._renderQA(model.getId(), self.ord, note.joinedFields(), self.id, note.id, self.originalDid(), note.stringTags(), self.flags, *args)
         return self._qa
 
     def note(self, reload=False):
