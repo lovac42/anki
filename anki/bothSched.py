1ce0125f386c3870f12f6aa3db1fedba169cac29
Arthur Milchior <Arthur@Milchior.fr>
2019-12-19 19:05:00 -0800

    bothSched.maybeRandomizeDeck

--- anki/bothSched.py
+++ anki/bothSched.py
@@ -761,3 +761,12 @@ and due >= ? and queue = {QUEUE_NEW}""" % (scids), now, self.col.usn(), shiftby,
                 self.randomizeCards(did)
             else:
                 self.orderCards(did)
+
+    # for post-import
+    def maybeRandomizeDeck(self, did=None):
+        if not did:
+            did = self.col.decks.selected()
+        conf = self.col.decks.confForDid(did)
+        # in order due?
+        if conf['new']['order'] == NEW_CARDS_RANDOM:
+            self.randomizeCards(did)
