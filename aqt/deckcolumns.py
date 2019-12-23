6f63118950b256103c3f8781c8fd80074ec1b0bc
Arthur Milchior <Arthur@Milchior.fr>
2019-12-22 20:09:05 -0800

    introduce deckColumn

---
+++ aqt/deckcolumns.py
@@ -0,0 +1,14 @@
+class Column:
+    pass
+
+class DeckName(Column):
+    pass
+
+class Number:
+    def __init__(self, name, key, color):
+        self.name = name
+        self.key = key
+        self.color = color
+
+class Gear(Column):
+    pass
