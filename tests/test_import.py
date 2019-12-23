67b330c412640555543796aed856bf1e18c592bc
Arthur Milchior <Arthur@Milchior.fr>
2019-12-22 16:50:47 -0800

    test importing every files

---
+++ tests/test_import.py
@@ -0,0 +1,28 @@
+import aqt
+from anki import (__init__, cards, collection, consts, db, decks, errors,
+                  exporting, find, hooks, lang, latex, media, models, mpv,
+                  notes, sched, schedv2, sound, stats, statsbg, stdmodels,
+                  storage, sync, tags, template, utils)
+from anki.importing import (__init__, anki2, apkg, base, csvfile, mnemo,
+                            noteimp, pauker, supermemo_xml)
+from anki.template import __init__, furigana, hint, template, view
+from anki.utils import intTime, isMac, isWin
+from aqt import (__init__, about, addcards, addons, browser, clayout,
+                 customstudy, deckbrowser, deckchooser, deckconf, downloader,
+                 dyndeckconf, editcurrent, editor, errors, exporting, fields,
+                 importing, main, mediasrv, modelchooser, models, overview,
+                 pinnedmodules, preferences, profiles, progress, qt, reviewer,
+                 sound, stats, studydeck, sync, tagedit, taglimit, toolbar,
+                 update, utils, webview)
+
+if isWin:
+    from aqt import winpaths
+
+
+
+
+
+
+
+def test_import():
+    pass
