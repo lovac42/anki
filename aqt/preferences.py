8a8fb8a61c91ba8738152ee4a014332ca4fbc2c4
Arthur Milchior <Arthur@Milchior.fr>
2020-01-01 02:27:30 -0800

    Adding preference: fontSize

--- aqt/preferences.py
+++ aqt/preferences.py
@@ -227,6 +227,8 @@ Not currently enabled; click the sync button in the main window to enable."""))
         {"name":"numBackups", "kind":"numeric", "sync":False},
         # advanced option
         {"name":"pastePNG", "sync":False},
+        # font
+        {"name":"fontSize", "kind":"numeric", "default":12},
     ]
 
     def setupOneSetting(self, name, default=False, kind="check", sync=True, fromCol=identity, toCol=identity):
